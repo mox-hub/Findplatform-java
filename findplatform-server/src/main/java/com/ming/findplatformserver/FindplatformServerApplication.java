7b7a5051b3288e28e8ca7ae4d8030525cb6753c3
魏思明 <3100328857@qq.com>
2021-09-19 10:11:15 +0800

    基础框架

---
+++ findplatform-server/src/main/java/com/ming/findplatformserver/FindplatformServerApplication.java
@@ -0,0 +1,14 @@
+package com.ming.findplatformserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class
+FindplatformServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FindplatformServerApplication.class, args);
+    }
+
+}
---
+++ findplatform-server/src/test/java/com/ming/findplatformserver/FindplatformServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ming.findplatformserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindplatformServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
