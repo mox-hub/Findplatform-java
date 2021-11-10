c93a27092e1f7d35778f6d9c348dadce12d03179
wsm123321 <86108330+wsm123321@users.noreply.github.com>
2021-11-10 17:06:51 +0800

    新建test项目

---
+++ findplatform-server-test/src/main/java/com/ming/findplatform/FindplatformServerTestApplication.java
@@ -0,0 +1,13 @@
+package com.ming.findplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindplatformServerTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FindplatformServerTestApplication.class, args);
+    }
+
+}
